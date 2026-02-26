6c7b93015a43e1f8690cbd4e7abcf4bff4ea11ea
smooker <miro@smooker.org>
2026-02-26 08:45:10 +0200

    fixed B2 line. 100k R4

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -57,264 +57,32 @@ uint32_t crcNew = 0;
 uint32_t iteration = 0;
 
 __attribute__((section(".romdata"))) const uint16_t arrFlash[256] = {
-    0x0000,
-    0x0001,
-    0x0002,
-    0x0003,
-    0x0004,
-    0x0005,
-    0x0006,
-    0x0007,
-    0x0008,
-    0x0009,
-    0x000a,
-    0x000b,
-    0x000c,
-    0x000d,
-    0x000e,
-    0x000f,
-    0x0010,
-    0x0011,
-    0x0012,
-    0x0013,
-    0x0014,
-    0x0015,
-    0x0016,
-    0x0017,
-    0x0018,
-    0x0019,
-    0x001a,
-    0x001b,
-    0x001c,
-    0x001d,
-    0x001e,
-    0x001f,
-    0x0020,
-    0x0021,
-    0x0022,
-    0x0023,
-    0x0024,
-    0x0025,
-    0x0026,
-    0x0027,
-    0x0028,
-    0x0029,
-    0x002a,
-    0x002b,
-    0x002c,
-    0x002d,
-    0x002e,
-    0x002f,
-    0x0030,
-    0x0031,
-    0x0032,
-    0x0033,
-    0x0034,
-    0x0035,
-    0x0036,
-    0x0037,
-    0x0038,
-    0x0039,
-    0x003a,
-    0x003b,
-    0x003c,
-    0x003d,
-    0x003e,
-    0x003f,
-    0x0040,
-    0x0041,
-    0x0042,
-    0x0043,
-    0x0044,
-    0x0045,
-    0x0046,
-    0x0047,
-    0x0048,
-    0x0049,
-    0x004a,
-    0x004b,
-    0x004c,
-    0x004d,
-    0x004e,
-    0x004f,
-    0x0050,
-    0x0051,
-    0x0052,
-    0x0053,
-    0x0054,
-    0x0055,
-    0x0056,
-    0x0057,
-    0x0058,
-    0x0059,
-    0x005a,
-    0x005b,
-    0x005c,
-    0x005d,
-    0x005e,
-    0x005f,
-    0x0060,
-    0x0061,
-    0x0062,
-    0x0063,
-    0x0064,
-    0x0065,
-    0x0066,
-    0x0067,
-    0x0068,
-    0x0069,
-    0x006a,
-    0x006b,
-    0x006c,
-    0x006d,
-    0x006e,
-    0x006f,
-    0x0070,
-    0x0071,
-    0x0072,
-    0x0073,
-    0x0074,
-    0x0075,
-    0x0076,
-    0x0077,
-    0x0078,
-    0x0079,
-    0x007a,
-    0x007b,
-    0x007c,
-    0x007d,
-    0x007e,
-    0x007f,
-    0x0080,
-    0x0081,
-    0x0082,
-    0x0083,
-    0x0084,
-    0x0085,
-    0x0086,
-    0x0087,
-    0x0088,
-    0x0089,
-    0x008a,
-    0x008b,
-    0x008c,
-    0x008d,
-    0x008e,
-    0x008f,
-    0x0090,
-    0x0091,
-    0x0092,
-    0x0093,
-    0x0094,
-    0x0095,
-    0x0096,
-    0x0097,
-    0x0098,
-    0x0099,
-    0x009a,
-    0x009b,
-    0x009c,
-    0x009d,
-    0x009e,
-    0x009f,
-    0x00a0,
-    0x00a1,
-    0x00a2,
-    0x00a3,
-    0x00a4,
-    0x00a5,
-    0x00a6,
-    0x00a7,
-    0x00a8,
-    0x00a9,
-    0x00aa,
-    0x00ab,
-    0x00ac,
-    0x00ad,
-    0x00ae,
-    0x00af,
-    0x00b0,
-    0x00b1,
-    0x00b2,
-    0x00b3,
-    0x00b4,
-    0x00b5,
-    0x00b6,
-    0x00b7,
-    0x00b8,
-    0x00b9,
-    0x00ba,
-    0x00bb,
-    0x00bc,
-    0x00bd,
-    0x00be,
-    0x00bf,
-    0x00c0,
-    0x00c1,
-    0x00c2,
-    0x00c3,
-    0x00c4,
-    0x00c5,
-    0x00c6,
-    0x00c7,
-    0x00c8,
-    0x00c9,
-    0x00ca,
-    0x00cb,
-    0x00cc,
-    0x00cd,
-    0x00ce,
-    0x00cf,
-    0x00d0,
-    0x00d1,
-    0x00d2,
-    0x00d3,
-    0x00d4,
-    0x00d5,
-    0x00d6,
-    0x00d7,
-    0x00d8,
-    0x00d9,
-    0x00da,
-    0x00db,
-    0x00dc,
-    0x00dd,
-    0x00de,
-    0x00df,
-    0x00e0,
-    0x00e1,
-    0x00e2,
-    0x00e3,
-    0x00e4,
-    0x00e5,
-    0x00e6,
-    0x00e7,
-    0x00e8,
-    0x00e9,
-    0x00ea,
-    0x00eb,
-    0x00ec,
-    0x00ed,
-    0x00ee,
-    0x00ef,
-    0x00f0,
-    0x00f1,
-    0x00f2,
-    0x00f3,
-    0x00f4,
-    0x00f5,
-    0x00f6,
-    0x00f7,
-    0x00f8,
-    0x00f9,
-    0x00fa,
-    0x00fb,
-    0x00fc,
-    0x00fd,
-    0x00fe,
-    0x00ff,
-};       //reserve space. KEEP in linker script!
-
+    0xcd74,  0x5edc,  0xcd74,  0x5edc,  0xcf64,  0x5edb,  0xcf74,  0x5eda,  0xcf74,  0x5eda,
+    0xcf74,  0x5eda,  0xcf74,  0x5eda,  0xcf74,  0x5eda,  0x5de5,  0x57cc,  0x5df5,  0x57cb,
+    0x5fe5,  0x57ca,  0x5ff5,  0x57c9,  0x5ded,  0x57ab,  0x5dfd,  0x57aa,  0x5fed,  0x57a9,
+    0x5ffd,  0x57a8,  0xdd2f,  0xe208,  0xdd2f,  0xe208,  0xdd2f,  0xe208,  0xdd2f,  0xe208,
+    0xdd2f,  0xe208,  0xdd2f,  0xe208,  0xdd2f,  0xe208,  0xdd2f,  0xe208,  0x5de4,  0x57af,
+    0x5df4,  0x57ae,  0x5fe4,  0x57ad,  0x5ff4,  0x57ac,  0x5dec,  0x57ab,  0x5dfc,  0x57aa,
+    0x5fec,  0x57a9,  0x5ffc,  0x57a8,  0xd9c5,  0x97cb,  0xd9d5,  0x97ca,  0xdbc5,  0x97ac,
+    0xdbd5,  0x97ab,  0xd9cd,  0x97aa,  0xd9dd,  0x97a9,  0xdbcd,  0x97a8,  0xdbdd,  0x97a7,
+    0xdd2f,  0xe208,  0xdd2f,  0xe208,  0xdd2f,  0xe208,  0xdd2f,  0xe208,  0xdd2f,  0xe208,
+    0xdd2f,  0xe208,  0xdd2f,  0xe208,  0xdd2f,  0xe208,  0x5df5,  0xf00f,  0x5fe5,  0xf00e,
+    0x5ff5,  0xf00d,  0x5ded,  0xf00c,  0x5dfd,  0xf00b,  0x5fed,  0xf00a,  0x5ffd,  0xf009,
+    0x7fbd,  0xf208,  0x7e24,  0xdbd9,  0x7e34,  0xdbda,  0x7c2c,  0xdbdb,  0x7c3c,  0xdba9,
+    0x7e2c,  0xdbaa,  0x7e3c,  0xdbab,  0x7ebc,  0xfb99,  0x7e1d,  0xfb9a,  0x5575,  0x798e,
+    0x5765,  0x798d,  0x5775,  0x798c,  0x556d,  0x798b,  0x557d,  0x798a,  0x576d,  0x7989,
+    0x577d,  0x799b,  0x7f1d,  0x7b9a,  0x7f24,  0x5bd9,  0x7f34,  0x5bda,  0x7d2c,  0x5bdb,
+    0x7d3c,  0x5ba9,  0x7f2c,  0x5baa,  0x7f3c,  0x5bab,  0x7fbc,  0x7b99,  0x7f1d,  0x7b9a,
+    0xee3c,  0xf20c,  0xee3c,  0xf20c,  0xee3c,  0xf20c,  0xee3c,  0xf20c,  0xee3c,  0xf20c,
+    0xee3c,  0xf20c,  0xee3c,  0xf20c,  0xee3c,  0xf20c,  0xdd65,  0x17cc,  0xdd75,  0x17cb,
+    0xdf65,  0x17ca,  0xdf75,  0x17c9,  0xdd6d,  0x17ab,  0xdd7d,  0x17aa,  0xdf6d,  0x17a9,
+    0xdf7d,  0x17a8,  0xcd74,  0x719d,  0xcf64,  0x719c,  0xcf74,  0x719b,  0xcd6c,  0x71ad,
+    0xcd7c,  0x71ac,  0xcf6c,  0x71ab,  0xcf7c,  0x700d,  0xef3c,  0x720c,  0x5d16,  0xd38a,
+    0x5f06,  0xd3a8,  0x5f16,  0xd3a9,  0x5d0e,  0xd3aa,  0x5d1e,  0xd3c8,  0x5f0e,  0xd3c9,
+    0x5f1e,  0xd3ca,  0xdd2f,  0xe208,  0x5d16,  0xf18a,  0x5f06,  0xf1a8,  0x5f16,  0xf1a9,
+    0x5d0e,  0xf1aa,  0x5d1e,  0xf1c8,  0x5f0e,  0xf1c9,  0x5f1e,  0xf1ca,  0xdd2f,  0xf208,
+    0x5d16,  0xe38a,  0x5f06,  0xe3a8,  0x5f16,  0xe3a9,  0x5d0e,  0xe3aa,  0x5d1e,  0xe3c8,
+    0x5f0e,  0xe3c9,  0x5f1e,  0xe3ca,  0xdd2f,  0xe208};
 
 /* USER CODE END PV */
 
@@ -410,7 +178,7 @@ void readProm2Ram()
         GPIOA->BSRR = addr32;
 
         // easy
-        // HAL_Delay(0);            //fixme
+        HAL_Delay(0);            //fixme
 
         //read whole 16 bits into arr
         arrRam[addr] = (uint16_t)GPIOB->IDR & 0x0000ffff;
@@ -428,7 +196,18 @@ void readProm2Ram()
 // emulate prom
 void emulateProm()
 {
+    while (1) {
+        uint32_t data32;
 
+        uint16_t data16;
+
+        uint8_t addr = (uint16_t)GPIOA->IDR & 0x000000ff;
+
+        data16 = arrFlash[addr];
+
+        data32 =  (~(data16 & 0xffff) << 16u) | (data16 & 0xffff);
+        GPIOB->BSRR = data32;
+    }
 }
 
 // reads ROM from FLASH to RAM
@@ -515,6 +294,7 @@ int main(void)
 {
 
   /* USER CODE BEGIN 1 */
+
   HAL_GPIO_WritePin(LED_USER_GPIO_Port, LED_USER_Pin, GPIO_PIN_RESET);
 
   // FLASH_Unlock();
@@ -557,15 +337,67 @@ int main(void)
   // BKPT;
 
   HAL_GPIO_WritePin(LED_USER_GPIO_Port, LED_USER_Pin, GPIO_PIN_RESET);
-  HAL_Delay(1000);
+  HAL_Delay(300);
   HAL_GPIO_WritePin(LED_USER_GPIO_Port, LED_USER_Pin, GPIO_PIN_SET);
-  HAL_Delay(1000);
+  HAL_Delay(300);
   HAL_GPIO_WritePin(LED_USER_GPIO_Port, LED_USER_Pin, GPIO_PIN_RESET);
-  HAL_Delay(1000);
+  HAL_Delay(300);
 
-  // GPIO2Emul();
+  if ( SW_RD_EM_SET ) {
+        // here goes emulation
+        GPIO2Emul();
+        emulateProm();
+  }
 
-  HAL_GPIO_WritePin(D12_GPIO_Port, D12_Pin, GPIO_PIN_SET);
+  if ( SW_RD_EM_RESET ) {
+        // here goes reading of prom
+        uint8_t i = 2;
+        while (i-- > 0 )
+        {
+            HAL_GPIO_WritePin(LED_USER_GPIO_Port, LED_USER_Pin, GPIO_PIN_RESET);
+            HAL_Delay(500);
+            HAL_GPIO_WritePin(LED_USER_GPIO_Port, LED_USER_Pin, GPIO_PIN_SET);
+            HAL_Delay(500);
+        }
+
+        //actual reading
+        i = 100;
+        while (i-- > 0) {
+            HAL_GPIO_WritePin(LED_USER_GPIO_Port, LED_USER_Pin, GPIO_PIN_RESET);
+            HAL_Delay(200);
+            HAL_GPIO_WritePin(LED_USER_GPIO_Port, LED_USER_Pin, GPIO_PIN_SET);
+            HAL_Delay(200);
+
+            readProm2Ram();
+            crcNew = crcCalcOfarrRam();
+            if ( (crcOld != 0x0) & (crcOld != crcNew) ) {
+                BKPT;
+            }
+            copyArrRam2ArrRamOld();
+            crcOld = crcNew;
+            iteration++;
+        }
+
+        while ( SW_RD_EM_RESET ) {
+            HAL_GPIO_WritePin(LED_USER_GPIO_Port, LED_USER_Pin, GPIO_PIN_RESET);
+            HAL_Delay(20);
+            HAL_GPIO_WritePin(LED_USER_GPIO_Port, LED_USER_Pin, GPIO_PIN_SET);
+            HAL_Delay(20);
+        }
+        // to flash
+        FLASH_Unlock();
+        FLASH_ErasePage(pageStartAddr);
+        i = 3;
+        while (i-- > 0 )
+        {
+            HAL_GPIO_WritePin(LED_USER_GPIO_Port, LED_USER_Pin, GPIO_PIN_RESET);
+            HAL_Delay(500);
+            HAL_GPIO_WritePin(LED_USER_GPIO_Port, LED_USER_Pin, GPIO_PIN_SET);
+            HAL_Delay(500);
+        }
+        writeRam2Flash();
+        HAL_GPIO_WritePin(LED_USER_GPIO_Port, LED_USER_Pin, GPIO_PIN_RESET);
+  }
 
   /* USER CODE END 2 */
 
@@ -573,37 +405,7 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    //
-    // if ( SW_RD_EM_SET ) {
-    //     HAL_GPIO_WritePin(LED_USER_GPIO_Port, LED_USER_Pin, GPIO_PIN_RESET);
-    // }
-    // if ( SW_RD_EM_RESET ) {
-    //     HAL_GPIO_WritePin(LED_USER_GPIO_Port, LED_USER_Pin, GPIO_PIN_SET);
-    // }
 
-    // // send to D0-D15
-    // uint16_t data = 0xffff;
-    // uint32_t data32 =  (~(data & 0xffff) << 16u) | (data & 0xffff);
-    // GPIOB->BSRR = data32;
-    // HAL_Delay(0);
-    // data = 0x0000;
-    // data32 =  (~(data & 0xffff) << 16u) | (data & 0xffff);
-    // GPIOB->BSRR = data32;
-    // HAL_Delay(0);
-
-    HAL_GPIO_WritePin(LED_USER_GPIO_Port, LED_USER_Pin, GPIO_PIN_RESET);
-    HAL_Delay(0);
-    HAL_GPIO_WritePin(LED_USER_GPIO_Port, LED_USER_Pin, GPIO_PIN_SET);
-    HAL_Delay(0);
-
-    readProm2Ram();
-    crcNew = crcCalcOfarrRam();
-    if ( (crcOld != 0x0) & (crcOld != crcNew) ) {
-            BKPT;
-    }
-    copyArrRam2ArrRamOld();
-    crcOld = crcNew;
-    iteration++;
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -731,7 +533,7 @@ static void MX_GPIO_Init(void)
                           |D15_Pin|D3_Pin|D4_Pin|D5_Pin
                           |D6_Pin|D7_Pin|D8_Pin|D9_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
--- go.sh
+++ go.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 arm-none-eabi-objdump -h ./build/prom_emulation.elf
+arm-none-eabi-objdump -D ./build/prom_emulation.elf
+arm-none-eabi-objdump -d ./build/prom_emulation.elf
+
 vim ./build/prom_emulation.map
